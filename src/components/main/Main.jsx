c138d6893cdaf5bf54315e63cc906a50780475bc
srujana <gurrala.srujana@outlook.com>
2023-05-11 12:38:58 +0200

    design changed

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+import "./Main.css";
 
 export default function Main() {
   return (
-    <div class='container'>
-      <Container fluid='md'>
-        <div class='alert alert-success' role='alert'>
-          <h4 class='alert-heading'>SEP BUSINESS</h4>
-          <p>Hello!!! Welcome!</p>
-          <p class='mb-0'>
-            Only Administration manager will be able to register other
-            employees.
-          </p>
-          <Row className='justify-content-md-center'>
-            <Link to='/Login'>Login</Link>
-          </Row>
-          <Row className='justify-content-md-center'>
-            <Link to='/Register'>Register</Link>
-          </Row>
-        </div>
-      </Container>
+    <div class="container-main">
+      <div class="container">
+        <Container>
+          <div className="login-content">
+            <h4>SEP BUSINESS</h4>
+            <p>Hello!!! Welcome!</p>
+            <p>
+              Only Administration manager will be able to register other
+              employees.
+            </p>
+            <Row>
+              <Link to="/Login">Login</Link>
+            </Row>
+            <Row>
+              <Link to="/Register">Register</Link>
+            </Row>
+          </div>
+        </Container>
+      </div>
     </div>
-  )
+  );
 }
